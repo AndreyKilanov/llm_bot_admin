a749a53ba852296dccd65761623f8d16b3823189
AndreyKilanov <8941519@mail.ru>
2026-02-07 13:14:33 +0500

    feat(services): export MusicService and music_service in __init__.py

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -2,6 +2,7 @@ from .history_service import HistoryService
 from .llm_service import LLMService
 from .settings_service import SettingsService
 from .user_service import UserService
+from .music_service import music_service, MusicService
 
-__all__ = ["UserService", "HistoryService", "SettingsService", "LLMService"]
+__all__ = ["UserService", "HistoryService", "SettingsService", "LLMService", "MusicService", "music_service"]
 
