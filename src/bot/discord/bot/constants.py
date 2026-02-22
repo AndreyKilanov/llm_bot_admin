ecb48fe95fb2bb1c83530e4caeadf21bb419518c
AndreyKilanov <8941519@mail.ru>
2026-02-23 03:47:58 +0500

    feat(discord): increase max search results limit

--- src/bot/discord/bot/constants.py
+++ src/bot/discord/bot/constants.py
@@ -3,6 +3,7 @@ from typing import Final
 # Инфраструктурные константы
 OPUS_PATH: Final[str] = "/usr/lib/x86_64-linux-gnu/libopus.so.0"
 DEFAULT_PREFIX: Final[str] = "/"
+MAX_SEARCH_RESULTS: Final[int] = 100
 
 # Визуальные элементы (эмодзи)
 ICON_ERR: Final[str] = "❌"
