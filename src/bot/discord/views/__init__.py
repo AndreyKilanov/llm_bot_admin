b4ff6b6f5c24ceeb42e71f5cbb158ac09b6b65f0
AndreyKilanov <8941519@mail.ru>
2026-02-22 18:20:26 +0500

    feat(discord): add views package initializer

---
+++ src/bot/discord/views/__init__.py
@@ -0,0 +1,11 @@
+from .track_selection import TrackSelectionView
+from .music_player import MusicPlayerView
+from .base import BaseMusicView, TrackInfo, DiscordEmoji
+
+__all__ = [
+    "TrackSelectionView",
+    "MusicPlayerView",
+    "BaseMusicView",
+    "TrackInfo",
+    "DiscordEmoji",
+]
